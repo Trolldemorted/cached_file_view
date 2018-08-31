59b7daf20310d945b18c34758b6ce6ee318b2552
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2018-08-31 22:27:26 +0200

    Track all yielded views

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::borrow::Borrow;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::fs::File;
@@ -5,118 +6,213 @@ use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
 use std::ops::Range;
-use std::cmp;
+use std::ops::Deref;
 use std::io;
+use std::collections::HashMap;
+use std::fmt;
 
-const BUFFERS_COUNT: usize = 3;
-const MINIMUM_BUFFER_SIZE: usize = 0x8000;
+const DEBUG: bool = false;
+const CHUNK_SIZE: usize = 0x8000;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum FileViewError {
     IOError,
-    EndOfFileError
+    OutOfBoundsError,
+    RangeTooBigError
 }
 
+#[derive(Debug, Clone)]
 pub struct FileView {
-    inner: Arc<Mutex<FileViewInner>>
+    inner: Arc<Mutex<FileViewInner>>,
+    pub length: u64
 }
 
+#[derive(Debug)]
 struct FileViewInner {
     file: File,
-    buffers: Vec<Arc<FileViewBuffer>>
+    buffers: HashMap<u64, FileViewChunkWrapper>
 }
 
-struct FileViewBuffer {
-    file_offset: u64,
+#[derive(Debug, Clone)]
+struct FileViewChunkWrapper {
+    readers: u64,
+    inner: Arc<FileViewChunk>
+}
+
+struct FileViewChunk {
+    offset: u64,
     data: Vec<u8>
 }
 
-pub struct FileViewItem {
-    buffer: Arc<FileViewBuffer>,
-    buffer_offset: u64,
-    len: u64
+#[derive(Clone)]
+pub struct FileViewMapping {
+    inner: Arc<FileViewMappingInner>
 }
 
-impl FileView {
-    pub fn new(file: File) -> Self {
-        FileView {
-            inner: Arc::new(Mutex::new(FileViewInner {
-                file: file,
-                buffers: vec!()
-            }))
-        }
-    }
+struct FileViewMappingInner {
+    file_view: FileView,
+    start_offset: usize,
+    end_offset: usize,
+    length: usize,
+    buffers: Vec<Arc<FileViewChunk>>
+}
 
-    pub fn len(&self) -> Result<u64, FileViewError> {
-        let inner = &mut *self.inner.lock().unwrap();
-        Ok(inner.file.metadata()?.len())
-    }
+pub struct FileViewMappingChunks<'a> {
+    mapping: &'a FileViewMapping,
+    index: usize
+}
 
-    pub fn read(&self, range: Range<u64>) -> Result<FileViewItem, FileViewError> {
-        let inner = &mut *self.inner.lock().unwrap();
+impl<'a> std::iter::Iterator for FileViewMappingChunks<'a> {
+    type Item = &'a [u8];
 
-        let buffer = if let Some(buffer) = inner.get(&range) {
-            buffer
-        } else {
-            // load a new chunk from disk
-            inner.file.seek(SeekFrom::Start(range.start))?;
-            let mut new_buffer = vec!(0;cmp::max(MINIMUM_BUFFER_SIZE, (range.end - range.start) as usize));
-            inner.file.read(&mut new_buffer)?;
-
-            // add it to our buffers
-            let file_view_buffer = Arc::new(FileViewBuffer {
-                file_offset: range.start,
-                data: new_buffer
-            });
-            if inner.buffers.len() < BUFFERS_COUNT {
-                inner.buffers.push(file_view_buffer.clone())
+    fn next(&mut self) -> Option<&'a [u8]> {
+        let index = self.index;
+        self.index += 1;
+        if index < self.mapping.inner.buffers.len() {
+            let begin = if index == 0 {
+                self.mapping.inner.start_offset
             } else {
-                inner.buffers[0] = file_view_buffer.clone()
-            }
-            file_view_buffer
-        };
-
-        if range.end - buffer.file_offset > buffer.data.len() as u64 || range.end > inner.file.metadata()?.len() {
-            return Err(FileViewError::EndOfFileError)
-        }
-
-        Ok(FileViewItem {
-            buffer_offset: range.start - buffer.file_offset,
-            buffer: buffer,
-            len: range.end - range.start
-        })
-    }
-}
-
-impl FileViewInner {
-    fn get(&mut self, range: &Range<u64>) -> Option<Arc<FileViewBuffer>> {
-        let len = self.buffers.len();
-        let hit_buffer = self.get_buffer(range);
-        if let Some((i, buffer)) = hit_buffer {
-            if i < len - 1 { // move hot buffer to the right
-                self.buffers.swap(i, i + 1);
-            }
-            Some(buffer)
+                0
+            };
+            let end = if index == self.mapping.inner.buffers.len()-1 {
+                self.mapping.inner.end_offset
+            } else {
+                CHUNK_SIZE
+            };
+            Some(&self.mapping.inner.buffers[index].data[begin..end])
         } else {
             None
         }
     }
+}
 
-    fn get_buffer(&self, range: &Range<u64>) -> Option<(usize, Arc<FileViewBuffer>)> {
-        for (i, buffer) in self.buffers.iter().enumerate() {
-            if buffer.file_offset + (buffer.data.len() as u64) >= range.end && buffer.file_offset <= range.start {
-                return Some((i, buffer.clone()))
-            }
+impl FileViewMapping {
+    pub fn chunks(&self) -> FileViewMappingChunks {
+        FileViewMappingChunks {
+            mapping: &self,
+            index: 0
         }
-        None
+    }
+
+    pub fn to_vec(&self) -> Vec<u8> {
+        let mut v = Vec::with_capacity(self.inner.length);
+        for chunk in self.chunks() {
+            v.extend_from_slice(chunk)
+        }
+        v
     }
 }
 
-impl FileViewItem {
-    pub fn get(&self) -> &[u8] {
-        let begin: usize = self.buffer_offset as usize;
-        let end: usize = (self.buffer_offset + self.len) as usize;
-        &self.buffer.data[begin..end]
+impl Drop for FileViewChunk {
+    fn drop(&mut self) {
+        if DEBUG {
+            println!("dropping {:?}", &self);
+        }
+    }
+}
+
+impl FileView {
+    pub fn new(file: File) -> Result<Self, FileViewError> {
+        Ok(FileView {
+            length: file.metadata()?.len(),
+            inner: Arc::new(Mutex::new(FileViewInner {
+                file: file,
+                buffers: HashMap::new()
+            }))
+        })
+    }
+
+    fn handle_dropped_mapping(&self, mapping: &FileViewMappingInner) {
+        let inner = &mut *self.inner.lock().unwrap();
+        let mut stale_buffer_ids = vec!();
+        for chunk in &mapping.buffers {
+            let wrapper = inner.buffers.get_mut(&chunk.offset).unwrap();
+            wrapper.readers -= 1;
+            if wrapper.readers == 0 {
+                stale_buffer_ids.push(chunk.offset)
+            }
+        }
+        for stale_buffer_id in stale_buffer_ids {
+            inner.buffers.remove(&stale_buffer_id).unwrap();
+        }
+    }
+
+    pub fn read<R: Borrow<Range<u64>>>(&self, range: R) -> Result<FileViewMapping, FileViewError> {
+        let inner = &mut *self.inner.lock().unwrap();
+        let range = range.borrow();
+        if DEBUG {
+            println!("read({:x}-{:x}) (self.length={:x})", range.start, range.end, self.length)
+        }
+        if range.end > self.length {
+            return Err(FileViewError::OutOfBoundsError);
+        }
+        let u64_len = range.end - range.start;
+        if u64_len > std::usize::MAX as u64 {
+            return Err(FileViewError::RangeTooBigError)
+        }
+        let len = u64_len as usize;
+        let start_offset = (range.start % CHUNK_SIZE as u64) as usize;
+        let mut end_offset;
+        let base_address = range.start - range.start % CHUNK_SIZE as u64;
+        let mut buffers = vec!();
+        let mut address = base_address;
+        inner.file.seek(SeekFrom::Start(base_address))?;
+        loop {
+            if inner.buffers.contains_key(&address) {
+                inner.file.seek(SeekFrom::Current(CHUNK_SIZE as i64))?;
+                let chunk = inner.buffers.get_mut(&address).unwrap();
+                chunk.readers += 1;
+                buffers.push(chunk.inner.clone())
+            } else {
+                let mut buffer = vec!(0; CHUNK_SIZE);
+                inner.file.read(&mut buffer)?;
+                let chunk = Arc::new(FileViewChunk {
+                    offset: address,
+                    data: buffer
+                });
+                inner.buffers.insert(address, FileViewChunkWrapper {
+                    readers: 1,
+                    inner: chunk.clone()
+                });
+                buffers.push(chunk);
+            }
+            end_offset = (range.end - address) as usize;
+            address += CHUNK_SIZE as u64;
+            if address >= range.end {
+                break;
+            }
+        }
+
+        Ok(FileViewMapping {
+            inner: Arc::new(FileViewMappingInner {
+                file_view: self.clone(),
+                start_offset: start_offset,
+                end_offset: end_offset,
+                length: len,
+                buffers: buffers
+            })
+        })
+    }
+}
+
+impl Drop for FileViewMappingInner {
+    fn drop(&mut self) {
+        if DEBUG {
+            println!("dropping {:?}", &self);
+        }
+        self.file_view.handle_dropped_mapping(&self);
+    }
+}
+
+impl fmt::Debug for FileViewMappingInner {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "FileViewMappingInner {{ start_offset: {:x}, length: {:x} }}", self.start_offset, self.length)
+    }
+}
+
+impl fmt::Debug for FileViewChunk {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "FileViewChunk {{ begin: {:x}, end: {:x} }}", self.offset, self.offset + CHUNK_SIZE as u64)
     }
 }
 
--- tests/lib.rs
+++ tests/lib.rs
@@ -5,27 +5,42 @@ use std::path::Path;
 use cached_file_view::FileView;
 use cached_file_view::FileViewError;
 
+/*
+#[test]
+fn basic() {
+    let f = File::open(Path::new("tests/test.txt")).expect("file not found");
+    let view1 = FileView::new(f).unwrap();
+    let view2 = view1.clone();
+    let full_mapping = view1.read(0..12).unwrap();
+    let small_mapping = view2.read(3..12).unwrap();
+
+    let full = full_mapping.to_vec();
+    let small = small_mapping.to_vec();
+    assert!(full == [104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33]);
+    assert!(small == [108, 111, 32, 119, 111, 114, 108, 100, 33]);
+}
+
 #[test]
 fn test_read1() {
     let f = File::open(Path::new("tests/test.txt")).expect("file not found");
-    let view = FileView::new(f);
-    assert!(view.read(0..12).unwrap().get() == "hello world!".as_bytes());
+    let view = FileView::new(f).unwrap();
+    assert!(view.read(0..12).unwrap().to_vec() == "hello world!".as_bytes());
 }
 
 #[test]
 fn test_read2() {
     let f = File::open(Path::new("tests/test.txt")).expect("file not found");
-    let view = FileView::new(f);
-    assert!(view.read(11..12).unwrap().get() == "!".as_bytes());
+    let view = FileView::new(f).unwrap();
+    assert!(view.read(11..12).unwrap().to_vec() == "!".as_bytes());
 }
 
 #[test]
 fn test_oob_end1() {
     let f = File::open(Path::new("tests/test.txt")).expect("file not found");
-    let view = FileView::new(f);
+    let view = FileView::new(f).unwrap();
 
     if let Err(err) = view.read(0..13) {
-        assert!(err == FileViewError::EndOfFileError);
+        assert!(err == FileViewError::OutOfBoundsError);
     } else {
         panic!("oob read did not fail")
     }
@@ -34,11 +49,26 @@ fn test_oob_end1() {
 #[test]
 fn test_oob_end2() {
     let f = File::open(Path::new("tests/test.txt")).expect("file not found");
-    let view = FileView::new(f);
+    let view = FileView::new(f).unwrap();
 
-    if let Err(err) = view.read(10..13) {
-        assert!(err == FileViewError::EndOfFileError);
+    if let Err(err) = view.read(&(10..13)) {
+        assert!(err == FileViewError::OutOfBoundsError);
     } else {
         panic!("oob read did not fail")
     }
+}
+*/
+#[test]
+fn test_huge() {
+    let f = File::open(Path::new("../twa_unpack/twa_rigidmodels.pack")).expect("file not found");
+    let view = FileView::new(f).unwrap();
+    {
+        let _ = view.read(0x0..0x4);
+        let _ = view.read(0x34..0x508DF);
+    }
+    
+    println!("#######################");
+    let v = view.read(0x508DF..0xE50FF).unwrap().to_vec();
+    println!("{:x?}", &v[0..4]);
+    assert!(&v[0..4] == "RMV2".as_bytes());
 }
